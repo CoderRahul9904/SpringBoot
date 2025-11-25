a3aafff1cac81f24d4712d36964a4ce0ed74a47b
Rahul <mouryarahul543@gmail.com>
2025-11-25 20:10:54 +0530

    changed the port

--- src/main/java/com/springboot/practice/PracticeApplication.java
+++ src/main/java/com/springboot/practice/PracticeApplication.java
@@ -1,11 +1,26 @@
 package com.springboot.practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class PracticeApplication implements CommandLineRunner {
 
+
+	private final RarozPaymentService paymentService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(paymentService.pay());
+		System.out.println("Payment Done");
+	}
+
+	public PracticeApplication(RarozPaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
