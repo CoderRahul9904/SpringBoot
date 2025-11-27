80e4560912bbbd4aa39b602431f2ed79bab6160e
Rahul <mouryarahul543@gmail.com>
2025-11-27 23:43:10 +0530

    added another getMapping "/student-all"

--- src/main/java/com/springboot/practice/controller/StudentController.java
+++ src/main/java/com/springboot/practice/controller/StudentController.java
@@ -2,14 +2,27 @@ package com.springboot.practice.controller;
 
 
 import com.springboot.practice.dto.StudentDto;
+import com.springboot.practice.entity.Student;
+import com.springboot.practice.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    private final StudentRepository studentRepository;
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
     @GetMapping("/student")
     public StudentDto getStudent(Integer sid){
         return new StudentDto(1,"Rahul",21);
+    }
+
+    @GetMapping("/student-all")
+    public List<Student> getAllStudent(Integer sid){
+        return studentRepository.findAll();
     }
 }
