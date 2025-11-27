35be9dfd0dc5c020af7e5eb4f5bf9768d8993228
Rahul <mouryarahul543@gmail.com>
2025-11-27 23:43:26 +0530

    update

--- src/main/java/com/springboot/practice/entity/Student.java
+++ src/main/java/com/springboot/practice/entity/Student.java
@@ -1,8 +1,12 @@
 package com.springboot.practice.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/springboot/practice/repository/StudentRepository.java
+++ src/main/java/com/springboot/practice/repository/StudentRepository.java
@@ -1,4 +1,9 @@
-package com.springboot.practice.reposistory;
+package com.springboot.practice.repository;
 
-public interface StudentRepository {
+import com.springboot.practice.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
