3f960df643b6a641488a43a0de38c8de44af5660
Rahul <mouryarahul543@gmail.com>
2025-11-29 01:18:57 +0530

    update

--- src/main/java/com/springboot/practice/service/impl/StudentServiceImpl.java
+++ src/main/java/com/springboot/practice/service/impl/StudentServiceImpl.java
@@ -5,6 +5,7 @@ import com.springboot.practice.entity.Student;
 import com.springboot.practice.repository.StudentRepository;
 import com.springboot.practice.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,9 +15,15 @@ import java.util.List;
 public class StudentServiceImpl implements StudentService {
 
     public final StudentRepository studentRepository;
-
+    public final ModelMapper modelMapper;
     @Override
     public List<StudentDto> getAllStudents() {
         return studentRepository.findAll().stream().map(student -> new StudentDto(student.getSid(),student.getSname(),student.getAge())).toList();
+    }
+
+    @Override
+    public StudentDto getStudentById(int id) {
+        Student student= studentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Student not found with Id"));
+        return modelMapper.map(student, StudentDto.class);
     }
 }
