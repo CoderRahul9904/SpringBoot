f2a4ed723b7a731878f5b4bb901cf2ffbeb136f1
Rahul <mouryarahul543@gmail.com>
2025-11-28 22:55:03 +0530

    add to line

--- src/main/java/com/springboot/practice/controller/StudentController.java
+++ src/main/java/com/springboot/practice/controller/StudentController.java
@@ -4,25 +4,25 @@ package com.springboot.practice.controller;
 import com.springboot.practice.dto.StudentDto;
 import com.springboot.practice.entity.Student;
 import com.springboot.practice.repository.StudentRepository;
+import com.springboot.practice.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
 
-    private final StudentRepository studentRepository;
-    public StudentController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
+    private final StudentService studentService;
     @GetMapping("/student")
     public StudentDto getStudent(Integer sid){
         return new StudentDto(1,"Rahul",21);
     }
 
     @GetMapping("/student-all")
-    public List<Student> getAllStudent(Integer sid){
-        return studentRepository.findAll();
+    public List<StudentDto> getAllStudent(Integer sid){
+        return studentService.getAllStudents();
     }
 }
