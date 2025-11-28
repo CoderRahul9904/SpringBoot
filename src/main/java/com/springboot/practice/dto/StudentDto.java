eefa36b0cb57ccf33c7b2cc7ee803f56711c5bc9
Rahul <mouryarahul543@gmail.com>
2025-11-29 01:18:44 +0530

    update

--- src/main/java/com/springboot/practice/dto/StudentDto.java
+++ src/main/java/com/springboot/practice/dto/StudentDto.java
@@ -2,8 +2,10 @@ package com.springboot.practice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class StudentDto {
     private int sid;
