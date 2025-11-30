cc2730fc45b014c86542d92a88d2de2580831683
Rahul <mouryarahul543@gmail.com>
2025-11-30 14:29:13 +0530

    update

--- src/main/java/com/springboot/practice/service/StudentService.java
+++ src/main/java/com/springboot/practice/service/StudentService.java
@@ -3,6 +3,7 @@ package com.springboot.practice.service;
 import com.springboot.practice.dto.AddStudentDto;
 import com.springboot.practice.dto.StudentDto;
 import com.springboot.practice.entity.Student;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -10,5 +11,6 @@ public interface StudentService {
     List<StudentDto> getAllStudents();
     StudentDto getStudentById(int id);
     StudentDto addNewStudent(AddStudentDto addStudentDto);
-    String deleteStudentById(int id);
+    ResponseEntity<Void> deleteStudentById(int id);
+    StudentDto updateStudentById(StudentDto studentDto);
 }
